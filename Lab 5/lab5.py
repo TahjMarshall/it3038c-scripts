566be0ccb09cfcebf1df475f890e460aa804920e
TahjMarshall <77473854+TahjMarshall@users.noreply.github.com>
2023-09-29 12:27:33 -0400

    Add files via upload

--- Lab 5/lab5.py
+++ Lab 5/lab5.py
@@ -1,1 +1,25 @@
+def count_letters_vowels_consonants(word):
+    letter_count = 0
+    vowel_count = 0
+    consonant_count = 0
 
+    vowels = set("aeiouAEIOU")
+
+    for char in word:
+        if char.isalpha():  
+            letter_count += 1
+            if char in vowels:
+                vowel_count += 1
+            else:
+                consonant_count += 1
+
+    return letter_count, vowel_count, consonant_count
+
+
+word = input("Type Something: ")
+
+letters, vowels, consonants = count_letters_vowels_consonants(word)
+
+print(f"Number of letters: {letters}")
+print(f"Number of vowels: {vowels}")
+print(f"Number of consonants: {consonants}")
