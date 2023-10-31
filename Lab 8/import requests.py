0528c2f93785384234053ce8a92db22c9f979b1c
TahjMarshall <77473854+TahjMarshall@users.noreply.github.com>
2023-10-31 11:48:25 -0400

    Add files via upload

---
+++ Lab 8/import requests.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+def get_amazon_product_details(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Get product title
+        title = soup.find(id='productTitle').get_text().strip()
+
+        # Get product price
+        price = None
+        price_span = soup.find(id='priceblock_ourprice')
+        if price_span:
+            price = price_span.get_text().strip()
+        else:
+            # If the above id doesn't work, you can try others like 'priceblock_dealprice'
+            pass
+        
+        return title, price
+    else:
+        print("Failed to retrieve the webpage. Status code:", response.status_code)
+        return None, None
+
+url = "https://www.amazon.com/TAOCOCO-Sectional-Furniture-Slipcovers-Pillowcases/dp/B07ZR6WV41/ref=asc_df_B07ZR6WV41/?tag=&linkCode=df0&hvadid=416790558681&hvpos=&hvnetw=g&hvrand=10269593383446226389&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9015705&hvtargid=pla-872502767463&ref=&adgrpid=90729934461&th=1"
+title, price = get_amazon_product_details(url)
+if title and price:
+    print(f"{title} - {price}")
+else:
+    print("Failed to extract product details.")
+
